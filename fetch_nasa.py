280af29c4d2a83ac26de85101bdce2aef6abe4b3
Migheli <98918370+Migheli@users.noreply.github.com>
2022-02-19 00:37:21 +0300

    Add files via upload

---
+++ fetch_nasa.py
@@ -0,0 +1,49 @@
+import requests
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+def img_saver(img_url, path_to_save):
+    response = requests.get(img_url)
+    response.raise_for_status()
+    with open(path_to_save, 'wb') as file:
+        file.write(response.content)
+
+
+def get_epic_imgs(api_key):
+    payload = {"api_key": api_key,
+               "images": "",
+               }
+    response = requests.get('https://api.nasa.gov/EPIC/api/natural', params=payload)
+    response.raise_for_status()
+    jsons = response.json()
+    print(len(jsons))
+    earth_urls = []
+    for jsn in jsons:
+        img_date, img_name = jsn['date'], jsn['image']
+        img_date = img_date.split(' ')[0]
+        year, month, day = img_date.split('-')
+        earth_url = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{img_name}.png?api_key={api_key}'
+        earth_urls.append(earth_url)
+    return earth_urls
+
+
+def fetch_epic_photos(earth_urls):
+    Path('images').mkdir(parents=True, exist_ok=True)
+    for img_index, earth_url in enumerate(earth_urls):
+        img_id = img_index + 1
+        path_to_save = f'images/earth{img_id}.png'
+        img_saver(earth_url, path_to_save)
+
+
+def main():
+    dotenv_path = 'dot.env'
+    load_dotenv(dotenv_path)
+    api_key = os.getenv('API_KEY')
+    epic_photos = get_epic_imgs(api_key)
+    fetch_epic_photos(epic_photos)
+
+
+if __name__ == '__main__':
+    main()
