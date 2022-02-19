eab3e31bf198d63119612f97313fc329a73b70e9
Migheli <98918370+Migheli@users.noreply.github.com>
2022-02-19 10:17:37 +0300

    Добавлена автоподгрузка фотографий при запуске

--- telebot.py
+++ telebot.py
@@ -12,6 +12,14 @@ def main():
 
     images = os.listdir('images')
 
+    api_key = os.getenv('API_KEY')
+
+    epic_photos = fetch_nasa.get_epic_imgs(api_key)
+    fetch_nasa.fetch_epic_photos(epic_photos)
+    img_urls = fetch_spacex.get_spacex_imgs(api_key, 25)
+    fetch_spacex.fetch_spacex_last_launch(img_urls)
+
+
     while True:
         for image in images:
             bot.send_photo(chat_id=chat_id, photo=open(f'images/{image}', 'rb'))
