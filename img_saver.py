66f6058262c7e5557f689c8bc587e2ea41c45a0c
Migheli <98918370+Migheli@users.noreply.github.com>
2022-02-21 22:23:29 +0300

    Delete img_saver.py

--- img_saver.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def save_img(img_url, path_to_save):
-    response = requests.get(img_url)
-    response.raise_for_status()
-    with open(path_to_save, 'wb') as file:
-        file.write(response.content)
